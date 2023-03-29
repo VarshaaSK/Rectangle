b545ab8e37b7c30baead6e2498916428554da965
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-29 15:58:50 +0530

    Add. Requirment 1 : Included the Logic and Test Case for Calculating the Area of Rectangle

--- src/main/java/com/m2p/AreaOfRectangle.java
+++ src/main/java/com/m2p/AreaOfRectangle.java
@@ -1,4 +1,10 @@
 package com.m2p;
 
 public class AreaOfRectangle {
+    public int findArea(int length, int width) {
+        if(length < 0 || width < 0){
+            throw new IllegalArgumentException();
+        }
+        return length * width;
+    }
 }
--- src/test/java/com/m2p/AreaOfRectangleTest.java
+++ src/test/java/com/m2p/AreaOfRectangleTest.java
@@ -1,7 +1,35 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AreaOfRectangleTest {
 
-    AreaOfRectangle areaOgRectangle = new AreaOfRectangle();
+    AreaOfRectangle areaOfRectangle = new AreaOfRectangle();
+
+    @Nested
+    class GeometricArea {
+        @Test
+        void toReturnAreaOfRectangle(){
+
+            //Arrange
+            int expectedArea = 6;
+
+            //Act
+            int actualArea = areaOfRectangle.findArea(2,3);
+
+            //Assert
+            assertEquals(expectedArea , actualArea);
+
+        }
+
+        @Test
+        void toThrowExceptionWhenOneParameterIsNegative(){
+            assertThrows(IllegalArgumentException.class , () -> areaOfRectangle.findArea(3,-2));
+        }
+    }
 
 }
